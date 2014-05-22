f5e7aeb76fc0a45372eb0783f3a41aef6a15a950
JonasThys <thysjonas1@gmail.com>
2014-05-22 16:00:43 +0200

    FireStatement

---
+++ src/worms/model/programs/FireStatement.java
@@ -0,0 +1,17 @@
+package worms.model.programs;
+
+public class FireStatement implements Statement {
+
+	private Expression theta;
+
+	public FireStatement(Expression theta){
+		this.theta = theta;
+	}
+	
+	@Override
+	public void execute() {
+		
+		
+	}
+
+}
